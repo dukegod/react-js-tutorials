05cd9a2b29b0cd61625f65e3e4aacd81b2e288e2
dukegod <dukehui1991@gmail.com>
2018-10-31 18:11:34 +0800

    redux 通信问题

--- 5-redux-react/src/js/components/profile.js
+++ 5-redux-react/src/js/components/profile.js
@@ -1,14 +1,22 @@
-import React from "react"
-import { connect } from "react-redux"
+import React from 'react';
+import { connect } from 'react-redux';
 
-@connect((store) => {
+@connect(store => {
   return {
-    user: store.user.user,
+    user: store.user.user
   };
 })
-export default class Profile extends React.Component {    
+export default class Profile extends React.Component {
   render() {
-    const { user} = this.props;
-    return <h2>姓名：{user.name}  ---- 年龄：{user.age}</h2>
+    console.log(this);
+    const { user } = this.props;
+    return (
+      <h2>
+        姓名：
+        {user.name}  
+        ---- 年龄：
+        {user.age} 
+      </h2>
+    );
   }
 }
--- 5-redux-react/src/js/components/view.js
+++ 5-redux-react/src/js/components/view.js
@@ -1,18 +1,28 @@
-import React from "react"
-import { connect } from "react-redux"
+import React from 'react';
+import { connect } from 'react-redux';
+import { setUserName } from "../actions/userActions"
 
-@connect((store) => {
+@connect(store => {
   return {
-    tweets: store.tweets.tweets,
+    // user: store.user.user,
+    tweets: store.tweets.tweets
   };
 })
-export default class ViewContent extends React.Component {     
-  
-  render() {
-    const { tweets } = this.props;
-    const mappedTweets = tweets.map(tweet => <li key={tweet.id}>{tweet.title}</li>)
-    return <div>
-      <ul>{mappedTweets}</ul>
-    </div>
+export default class ViewContent extends React.Component {
+  setUserName() {
+    this.props.dispatch(setUserName('liuhui_is_me'))
   }
-};
+  render() {
+    console.log(this)
+    const { tweets } = this.props;
+    const mappedTweets = tweets.map(tweet => (
+      <li key={tweet.id}>{tweet.title}</li>
+    ));
+    return (
+      <div>
+        <ul>{mappedTweets}</ul>
+        <button onClick={this.setUserName.bind(this)}>子组件控制父组件的名字显示</button>
+      </div>
+    );
+  }
+}
